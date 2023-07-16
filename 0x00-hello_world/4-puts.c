2229938739af02d90bfc0465f888252e5f804b1f
Your Name <you@example.com>
2023-07-15 17:30:18 -0700

    p

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
-#include <stdio.h>
-
-void main()
+#include<stdio.h>
+int main(void)
 {
-	char message[] = "\"Programming is like building a multilingual puzzle";
-
-	puts("%d\n, message");
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
 }

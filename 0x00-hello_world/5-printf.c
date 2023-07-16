7b15d15262cf8d9c49239d056920c75776316dc8
Your Name <you@example.com>
2023-07-16 12:03:35 -0700

    o

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-int main()
+/**
+ * main - Entry point
+ *
+ * Description: a c program that print with printf function
+ *
+ * return: Always 0 (success)
+ *
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return 0;
+	return (0);
 }

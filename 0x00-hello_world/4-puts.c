3cc47d94855708726734b9e9892858841c7e915a
Your Name <you@example.com>
2023-07-16 06:56:24 -0700

    ^^

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main : starting point
+ * main - Entry point
  *
- * description : this code will input
+ * Description : this is a c program that prints with puts function
  *
- * return : always 0 (success)
+ * return: Always 0 (success)
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }

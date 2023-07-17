ae3a29e432611f135d938e48e6f3147dde9ab55f
Your Name <you@example.com>
2023-07-17 16:00:41 -0700

    k

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - starting function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char character = 1;
+	int integer = 4;
+	long int inteeeger = 4;
+	long long int inteeeeger = 8;
+	float floating = 4;
+printf("%d\n,character");
+printf("%d\n,integer");
+printf("%d\n,inteeeger");
+printf("%d\n,inteeeeger");
+printf("%d\n,floating");
+return (0);
+}

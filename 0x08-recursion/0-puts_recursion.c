351f7297c00d266159e2ffaa2f183664af6e2428
Your Name <you@example.com>
2023-10-01 07:48:32 -0700

    o

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main : print a string
+ * Return always (0);
+ */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0') {
-        putchar('\n');  // Print a newline when the end of the string is reached
-        return;
-    }
-
-    putchar(*s);  // Print the current character
-    _puts_recursion(s + 1);  // Move to the next character and recurse
-}
-
-int main()
-{
-    char str[] = "Hello, world!";
-    _puts_recursion(str);
-
-    return 0;
+    if(*s == '\0'){
+        putchar ('\n');
+    return;}
+    putchar(*s);
+    _puts_recursion(s+1);
 }

df87123a8e1b681482b9da979aa8791e27191993
Your Name <you@example.com>
2023-10-01 08:35:59 -0700

    o

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* _print_rev_recursion - Prints a string followed by a new line
+* @s: string
+*
+* Return: On success 1.
+* On error, -1 is returned, and erro is set appropriately.
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	putchar('\n');
+	return;
+	}
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}

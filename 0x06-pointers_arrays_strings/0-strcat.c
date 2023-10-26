3f03e5a48edbe80fb0c7fc8054c49a3dd342c8a9
Your Name <you@example.com>
2023-10-26 14:32:09 -0700

    o

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+* _strcat - concatenates 2 strings.
+* @dest: string with concatenation
+* @src: string to be concatenated
+* Return: Always 0.
+*/
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest,src);
+	return dest;
+}
